10772875087540a0ce71a0d1cf01050a417472bc
Justine Casiano <justinecasiano30@gmail.com>
2023-08-18 12:26:21 +0800

    update git automation script

--- run_me.sh
+++ run_me.sh
@@ -2,13 +2,14 @@
 # not needed here so I removed it.
 
 # ask user for input 
-while [ true ]; do
-    echo "What do you like to do?
-    1. Save my task
-    2. Show save history"
+while true; do
+    echo "What would you like to do?
+    1. Get changes
+    2. Save my task
+    3. Show save history"
     read -p "Select an option: " option
 
-    if (( option >= 1 && option <= 2)); then break; fi
+    if (( option >= 1 && option <= 3)); then break; fi
     clear
 done
 
@@ -20,8 +21,17 @@ if [[ $? -ne 0 ]]; then
 fi
 
 # function to delete unused and already pushed branches
-(git fetch -p && git branch --merged | grep -v '*' \
-| grep -v 'main' | xargs git branch -d) 2>> ./logs/error-logs.txt > ./logs/output-logs.txt 
+(git fetch -p && git branch -a | grep -vE '\*|main|head' | xargs git branch -D) 2>> ./logs/error-logs.txt > ./logs/output-logs.txt 
+
+# function to get latest code changes from github
+get_changes () 
+{
+    (git stash -u && git switch main \
+        && git pull --rebase origin main \
+            && git stash apply) \
+                2> ./logs/error-logs.txt \
+                > ./logs/output-logs.txt 
+}
 
 # function to save changes, pull latest code from github then 
 # push your changes to github
@@ -32,10 +42,9 @@ save_task ()
     if [[ "${#task}" -ne  0 ]]; then 
         (git switch -c "feature/${task}" \
             && git add . && git commit -m "$message" \
-                && git pull --rebase origin main \
-                    && git push -u origin "feature/${task}") \
-                        2> ./logs/error-logs.txt \
-                        > ./logs/output-logs.txt 
+                && git push -u origin "feature/${task}") \
+                    2> ./logs/error-logs.txt \
+                    > ./logs/output-logs.txt 
     else
         clear; save_task
     fi
@@ -44,17 +53,17 @@ save_task ()
 # executes function/command based on what is entered by user
 # default option is to show the commit history 
 case $option in
-    1) save_task;;
-    *) git log --oneline --graph --decorate --pretty="format: %h: %ar - %s (%an) %d"
+    1) get_changes;;
+    2) save_task;;
+    3) git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)'
 esac
-   
+
 # check if there's an error encountered while executing git commands
 if [[ $? -eq 0 ]]; then
-    echo "Success command execution, you may now proceed..."
-
-    if (( option == 1 )); then
-        echo "Tell Just to create a pull request with your task."
-    fi
+    case $option in
+        1) echo "Success updating the codebase with latest changes. You may now proceed with your task.";; 
+        2) echo "Success saving changes. Tell Just to create a pull request with your task.";;
+    esac
 else
-    echo "Encountered an error, stopping execution. Report it to Just."
+    echo "Encountered an error, stopping execution. Check error logs and report it to Just."
 fi
